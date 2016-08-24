aff32aa25fde5b24d5e42f27194fe08667c1731f
satyapriya <subudhisatyapriya@gmail.com>
2016-08-24 12:21:35 +0530

    fix statusbar not getting transparent

--- src/android/StatusbarTransparent.java
+++ src/android/StatusbarTransparent.java
@@ -18,6 +18,7 @@ public class StatusbarTransparent extends CordovaPlugin {
 			if(VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
 				cordova.getActivity().runOnUiThread( new Runnable() {
 					public void run() {
+						cordova.getActivity().getWindow().clearFlags(LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 						cordova.getActivity().getWindow().addFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
 					}
 				});
